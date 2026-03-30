964ba4850dbca1ea4c15be9a8731681a4b7a0abb
Remiltan123 <regiremiltan2002@gmail.com>
2026-03-30 09:58:32 -0700

    inital config for eureka server

---
+++ eureka-server/src/main/java/com/example/demo/Environment.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Environment {
+
+    public String getProperty(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProperty'");
+    }
+
+}
